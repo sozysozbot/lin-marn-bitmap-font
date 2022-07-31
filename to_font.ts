742970e64d7624e3f161a218a2c15228de4553af
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2022-07-31 22:05:29 +0900

    Update to_font.ts and its dependencies

--- to_font.ts
+++ to_font.ts
@@ -1,35 +1,35 @@
 import { generateFonts, FontAssetType, OtherAssetType } from 'fantasticon';
 import * as fs from 'fs';
 import * as fs_extra from 'fs-extra'
-(async function() {
-const fix_path = process.argv[3] ?? "imgs"
-const out_path = process.argv[4] ?? "fonts"
-const glyph_map: { [key: string]: number } = {};
-const files = fs.readdirSync(`${fix_path}/`);
-files.forEach((file, index) => {
-  if (file.slice(-4) !== ".svg") return;
-  glyph_map[file[0]] = file.charCodeAt(0);
-
-  const svg_path = `${fix_path}/${file}`;
-  const svg_glyph = fs.readFileSync(svg_path, 'utf-8').replace("0 0 136 120", `0 0 600 ${600/136*120}`);
-  fs.writeFileSync(svg_path, svg_glyph, 'utf-8');
-});
-generateFonts({
-  inputDir: `./${fix_path}`,
-  outputDir: `./${out_path}`,
-  name: "lin-marn-bitmap-font",
-  fontTypes: [FontAssetType.TTF, FontAssetType.WOFF],
-  assetTypes: [
-    OtherAssetType.CSS,
-    OtherAssetType.HTML,
-    OtherAssetType.JSON,
-   /* OtherAssetType.TS */ // The TS asset is buggy; remove
-  ],
-  fontHeight: 480,
-  codepoints: glyph_map
-}).then(results => {
-  console.log(results);
-  // copy the resulting fonts into docs/
-  fs_extra.copy("fonts", "docs/fonts");
-});
+(async function () {
+  const in_path = process.argv[3] ?? "imgs"
+  const out_path = process.argv[4] ?? "fonts"
+  const glyph_map: { [key: string]: number } = {};
+  const files = fs.readdirSync(`${in_path}/`);
+  files.forEach((file, index) => {
+    if (file.slice(-4) !== ".svg") return;
+    if (file === "_colon.svg") {
+      glyph_map[":"] = ":".charCodeAt(0);
+    } else {
+      glyph_map[file[0]] = file.charCodeAt(0);
+    }
+  });
+  generateFonts({
+    inputDir: `./${in_path}`,
+    outputDir: `./${out_path}`,
+    name: "lin-marn-bitmap-font",
+    fontTypes: [FontAssetType.TTF, FontAssetType.WOFF],
+    assetTypes: [
+      OtherAssetType.CSS,
+      OtherAssetType.HTML,
+      OtherAssetType.JSON,
+      /* OtherAssetType.TS */ // The TS asset is buggy; remove
+    ],
+    fontHeight: 480,
+    codepoints: glyph_map
+  }).then(results => {
+    console.log(results);
+    // copy the resulting fonts into docs/
+    fs_extra.copy("fonts", "docs/fonts");
+  });
 })();
